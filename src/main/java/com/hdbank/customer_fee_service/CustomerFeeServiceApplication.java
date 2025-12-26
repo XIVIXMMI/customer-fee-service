70d4e592e415458143f7a10a37e2cbbd679cb437
XIVIXMMI <lethaikhoinguyen@gmail.com>
2025-12-26 14:53:12 +0700

    chore: initial project setup

---
+++ src/main/java/com/hdbank/customer_fee_service/CustomerFeeServiceApplication.java
@@ -0,0 +1,13 @@
+package com.hdbank.customer_fee_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerFeeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerFeeServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hdbank/customer_fee_service/CustomerFeeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hdbank.customer_fee_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerFeeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
